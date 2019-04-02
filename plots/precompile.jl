0fb29326988e3b942a883b6f31e9820ae52348c9
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-01 21:50:00 -0700

    Precompile show for Plots

--- plots/precompile.jl
+++ plots/precompile.jl
@@ -4,7 +4,8 @@ Base.include(Main, joinpath(@__DIR__, "..", "lib", "precompile_preamble.jl"))
 import Plots
 
 let xs = ones(3)
-    Plots.plot(xs)
-    Plots.histogram(xs)
-    Plots.scatter(xs)
+    mime = MIME("image/png")
+    show(devnull, mime, Plots.plot(xs))
+    show(devnull, mime, Plots.histogram(xs))
+    show(devnull, mime, Plots.scatter(xs))
 end
