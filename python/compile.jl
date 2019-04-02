d96242ad5aac4612674e261cf586b6466acd8a6d
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-01 20:15:29 -0700

    Add a recipe "python"

---
+++ python/compile.jl
@@ -0,0 +1,13 @@
+sysout, = ARGS
+outdir = dirname(sysout)
+
+using PackageCompiler
+
+systmp, _curr_syso = compile_incremental(
+    joinpath(outdir, "Project.toml"),
+    joinpath(@__DIR__, "precompile.jl"),
+)
+
+cp(systmp, sysout, force=true)
+
+write(joinpath(outdir, "julia_executable"), Base.julia_cmd().exec[1])
