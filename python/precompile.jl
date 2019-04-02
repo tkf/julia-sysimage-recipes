ef113824f3b69ca3f9e7c2083d06a142ef3526df
Takafumi Arakaki <aka.tkf@gmail.com>
2019-04-01 20:53:54 -0700

    Some precompilation for IPython

--- python/precompile.jl
+++ python/precompile.jl
@@ -1,2 +1,9 @@
 # include: ../lib/precompile_preamble.jl
 Base.include(Main, joinpath(@__DIR__, "..", "lib", "precompile_preamble.jl"))
+
+import IPython
+precompile(IPython.start_ipython, ())
+precompile(IPython.JuliaAPI.eval_str, (String,))
+precompile(IPython.JuliaAPI.completions, (String, Int, Module))
+precompile(IPython.JuliaAPI.completions, (String, Int))
+precompile(IPython.init_repl, (IPython.REPL.LineEditREPL,))
