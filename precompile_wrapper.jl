43efb468c078d223f4ca4dac9af2b771cdce38c6
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-15 11:34:28 -0700

    Fix precompile_wrapper.jl

--- precompile_wrapper.jl
+++ precompile_wrapper.jl
@@ -1,4 +1,4 @@
-# include: ../lib/precompile_preamble.jl
-Base.include(Main, joinpath(@__DIR__, "..", "lib", "precompile_preamble.jl"))
+# include: lib/precompile_preamble.jl
+Base.include(Main, joinpath(@__DIR__, "lib", "precompile_preamble.jl"))
 
 Base.include(Main, "precompile.jl")
