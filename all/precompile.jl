c10311ab6d8bc492b104e2a458a200fcc6f1b790
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-15 11:05:36 -0700

    Remove all recipes but plots/

--- all/precompile.jl
+++
@@ -1,2 +0,0 @@
-Base.include(Main, joinpath(@__DIR__, "..", "python", "precompile.jl"))
-Base.include(Main, joinpath(@__DIR__, "..", "plots", "precompile.jl"))
--- rebugger/test.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-try
-    Pkg.test("Rebugger")
-catch exception
-    @error "Ignoring Rebugger test failures" exception
-end
-Pkg.test(["Revise"])
