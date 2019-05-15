508f24309df69d1de24729ae383914b743be62ba
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-15 10:56:23 -0700

    Remove all recipes but python/

--- plots/precompile.jl
+++
@@ -1,8 +0,0 @@
-import Plots
-
-let xs = ones(3)
-    mime = MIME("image/png")
-    show(devnull, mime, Plots.plot(xs))
-    show(devnull, mime, Plots.histogram(xs))
-    show(devnull, mime, Plots.scatter(xs))
-end
