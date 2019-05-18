ef09ec498e37dc79287ceffdc9df14746cb65f87
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-18 01:01:07 -0700

    Run some code using Pandas and DataFrames

--- precompile.jl
+++ precompile.jl
@@ -4,3 +4,9 @@ precompile(IPython.JuliaAPI.eval_str, (String,))
 precompile(IPython.JuliaAPI.completions, (String, Int, Module))
 precompile(IPython.JuliaAPI.completions, (String, Int))
 precompile(IPython.init_repl, (IPython.REPL.LineEditREPL,))
+
+import DataFrames
+import Pandas
+df = DataFrames.DataFrame(x=[1, 2, 3], y=[1.0, 2.0, 3.0])
+display(DataFrames.DataFrame(Pandas.DataFrame(df)))
+display(Pandas.DataFrame(df))
