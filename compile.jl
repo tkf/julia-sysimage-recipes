86316dbd698ce9500237a7df0d44b7aef85df93a
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-19 18:01:33 -0700

    Stop using PackageCompiler.snoop_packages

--- compile.jl
+++ compile.jl
@@ -3,18 +3,9 @@ outdir = dirname(sysout)
 
 using PackageCompiler
 
-_toml, precompile = PackageCompiler.snoop_packages(:Makie, :AbstractPlotting)
-
-wrapper = joinpath(@__DIR__, "build", "precompile_wrapper-$VERSION.jl")
-preamble = joinpath(@__DIR__, "lib", "precompile_preamble.jl")
-write(wrapper, """
-Base.include(Main, $(repr(preamble)))
-Base.include(Main, $(repr(precompile)))
-""")
-
 systmp, _curr_syso = compile_incremental(
     joinpath(outdir, "Project.toml"),
-    wrapper,
+    joinpath(@__DIR__, "precompile_wrapper.jl"),
 )
 
 cp(systmp, sysout, force=true)
