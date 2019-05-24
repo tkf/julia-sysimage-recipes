336482c238f269f6fbd18654d4ac57ec2b576084
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-23 21:51:57 -0700

    Create recipe 'base'

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Travis Status][travis-img]][travis-url]
 
-[travis-img]: https://travis-ci.com/tkf/julia-sysimage-recipes.svg?branch=python/master
+[travis-img]: https://travis-ci.com/tkf/julia-sysimage-recipes.svg?branch=base/master
 [travis-url]: https://travis-ci.com/tkf/julia-sysimage-recipes
--- precompile.jl
+++
@@ -1,6 +0,0 @@
-import IPython
-precompile(IPython.start_ipython, ())
-precompile(IPython.JuliaAPI.eval_str, (String,))
-precompile(IPython.JuliaAPI.completions, (String, Int, Module))
-precompile(IPython.JuliaAPI.completions, (String, Int))
-precompile(IPython.init_repl, (IPython.REPL.LineEditREPL,))
