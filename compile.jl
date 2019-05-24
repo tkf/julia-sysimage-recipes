16764341aba41aa79c9d63a65ec96848ab044b82
Takafumi Arakaki <aka.tkf@gmail.com>
2019-05-24 00:04:32 -0700

    Use snoop_packages

--- compile.jl
+++ compile.jl
@@ -3,9 +3,18 @@ outdir = dirname(sysout)
 
 using PackageCompiler
 
+_toml, precompile = PackageCompiler.snoop_packages(:Flux)
+
+wrapper = joinpath(@__DIR__, "build", "precompile_wrapper-$VERSION.jl")
+patch = joinpath(@__DIR__, "lib", "jlm", "patch.jl")
+write(wrapper, """
+Base.include(Main, $(repr(patch)))
+Base.include(Main, $(repr(precompile)))
+""")
+
 systmp, _curr_syso = compile_incremental(
     joinpath(outdir, "Project.toml"),
-    joinpath(@__DIR__, "precompile_wrapper.jl"),
+    wrapper,
 )
 
 cp(systmp, sysout, force=true)
